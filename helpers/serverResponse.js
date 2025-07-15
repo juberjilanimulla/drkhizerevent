0476b46fd07884bd778e1e1f266f6e0a6f8d6af3
Juber_Mulla <juberjilanimulla@gmail.com>
2025-07-15 16:26:35 +0530

    this second commit today

--- helpers/serverResponse.js
+++ helpers/serverResponse.js
@@ -1,18 +1,17 @@
-
-export function succcessResponse(res,message,data=null){
-	res.status(200).json({
-		status:200,
-		error:false,
-		message,
-		data
-	})
+export function succcessResponse(res, message, data = null) {
+  res.status(200).json({
+    status: 200,
+    error: false,
+    message,
+    data,
+  });
 }
 
-export function errorResponse(res,statusCode,message){
-	res.status(statusCode).json({
-		status:statusCode,
-		error:true,
-		message,
-		data:null,
-	})
+export function errorResponse(res, statusCode, message) {
+  res.status(statusCode).json({
+    status: statusCode,
+    error: true,
+    message,
+    data: null,
+  });
 }
---
+++ routes/auth/authRouter.js
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import {
+  errorResponse,
+  succcessResponse,
+} from "../../helpers/serverResponse.js";
+import usermodel from "../../model/usermodel.js";
+
+const authRouter = Router();
+
+export default authRouter;
+
+async function signinHandler(req, res) {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return errorResponse(res, 400, "some params are missing");
+    }
+    const user = await usermodel.findOne({ email });
+    if (!user) {
+      return errorResponse(res, 404, "user is not exist");
+    }
+    const 
+  } catch (error) {
+    console.log("error", error);
+    errorResponse(res, 500, "internal server error");
+  }
+}
