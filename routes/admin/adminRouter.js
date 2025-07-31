8b94a255314cf4ce18fff689bf07cba45d35a41b
Juber_Mulla <juberjilanimulla@gmail.com>
2025-07-31 10:49:26 +0530

    this is commit this is contact

--- routes/admin/adminRouter.js
+++ routes/admin/adminRouter.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import admindoctorRouter from "./admindoctorRouter.js";
 import adminblogRouter from "./adminblogRouter.js";
+import admincontactRouter from "./admincontactRouter.js";
 
 const adminRouter = Router();
 
 adminRouter.use("/doctor", admindoctorRouter);
 adminRouter.use("/blog", adminblogRouter);
+adminRouter.use("/contact", admincontactRouter);
 
 export default adminRouter;
