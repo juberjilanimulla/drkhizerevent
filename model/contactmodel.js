9f72527e5233341e0b51282b3010227ece7fed3e
Juber_Mulla <juberjilanimulla@gmail.com>
2025-07-31 10:34:48 +0530

    this commit contact

---
+++ model/contactmodel.js
@@ -0,0 +1,39 @@
+import { Schema, model } from "mongoose";
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      default: "",
+    },
+    mobile: {
+      type: String,
+      default: "",
+    },
+    email: String,
+    message: String,
+  },
+  { timestamps: true, versionKey: false }
+);
+
+function currentLocalTimePlusOffset() {
+  const now = new Date();
+  const offset = 5.5 * 60 * 60 * 1000;
+  return new Date(now.getTime() + offset);
+}
+
+contactSchema.pre("save", function (next) {
+  const currentTime = currentLocalTimePlusOffset();
+  this.createdAt = currentTime;
+  this.updatedAt = currentTime;
+  next();
+});
+
+contactSchema.pre("findOneAndUpdate", function (next) {
+  const currentTime = currentLocalTimePlusOffset();
+  this.set({ updatedAt: currentTime });
+  next();
+});
+
+const contactmodel = model("contact", contactSchema);
+export default contactmodel;
