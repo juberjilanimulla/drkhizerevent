e08c7a14f8dc323e4fd28d2c79842cc76ab940db
Juber_Mulla <juberjilanimulla@gmail.com>
2025-08-01 11:24:18 +0530

    this some missing admin

--- routes/admin/adminRouter.js
+++ routes/admin/adminRouter.js
@@ -3,6 +3,7 @@ import admindoctorRouter from "./admindoctorRouter.js";
 import adminblogRouter from "./adminblogRouter.js";
 import admincontactRouter from "./admincontactRouter.js";
 import adminsubcribeRouter from "./adminsubcribeRouter.js";
+import adminreviewRouter from "./adminreviewRouter.js";
 
 const adminRouter = Router();
 
@@ -10,5 +11,6 @@ adminRouter.use("/doctor", admindoctorRouter);
 adminRouter.use("/blog", adminblogRouter);
 adminRouter.use("/contact", admincontactRouter);
 adminRouter.use("/subcribe", adminsubcribeRouter);
+adminRouter.use("/review", adminreviewRouter);
 
 export default adminRouter;
