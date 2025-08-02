4226fd5b1b581a57652401c00c72b131178bb9bb
Juber_Mulla <juberjilanimulla@gmail.com>
2025-08-02 14:49:41 +0530

    this is user

--- routes/user/userRouter.js
+++ routes/user/userRouter.js
@@ -4,6 +4,7 @@ import userblogRouter from "./userblogRouter.js";
 import usercontactRouter from "./usercontactRouter.js";
 import usersubcribeRouter from "./usersubcribeRouter.js";
 import userreviewRouter from "./userreviewRouter.js";
+import usercommentRouter from "./usercommentRouter.js";
 
 const userRouter = Router();
 
@@ -12,5 +13,6 @@ userRouter.use("/blog", userblogRouter);
 userRouter.use("/contact", usercontactRouter);
 userRouter.use("/subcribe", usersubcribeRouter);
 userRouter.use("/review", userreviewRouter);
+userRouter.use("/comment", usercommentRouter);
 
 export default userRouter;
