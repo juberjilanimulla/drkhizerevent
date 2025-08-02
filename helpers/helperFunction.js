02a36a09ec1f823b7f047c20673a90f4f60cb98b
Juber_Mulla <juberjilanimulla@gmail.com>
2025-08-02 13:07:11 +0530

    this is mujtaba password given dr

--- helpers/helperFunction.js
+++ helpers/helperFunction.js
@@ -130,7 +130,7 @@ export async function Admin() {
         email: email,
         role: "admin",
         mobile: 9966470788,
-        password: bcryptPassword("Khizer@0832"),
+        password: bcryptPassword("Drkhizer0832#*"),
       });
     } else {
     }
