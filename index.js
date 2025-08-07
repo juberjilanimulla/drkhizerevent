241ef19c009f79181b6b7f0d7256c11c6e4d0671
Juber_Mulla <juberjilanimulla@gmail.com>
2025-08-07 14:35:02 +0530

    this is vercel added in the cors

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 app.use(
   cors({
-    origin: ["https://drkhizerjunaidy.com"],
+    origin: ["https://drkhizerjunaidy.com", "https://drkhizer.vercel.app"],
     credentials: true,
   })
 );
